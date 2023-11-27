955e6f855e20eeb42130eaf650fa6f5d44fc00d5
mapadilla1031 <55124740+mapadilla1031@users.noreply.github.com>
2023-11-26 22:48:14 -0700

    Part 1 Initial Setup

--- PA3.py
+++ PA3.py
@@ -1,2 +1,8 @@
-print ("hello world")
-print("Please add code here.")
+#States
+states = ['R','T','D','U','8p']
+#Actions 
+actions = ['P', 'R', 'S','any']
+
+#need method to get possible conbinations of rested-party ,rest,study,any
+#agent follows a random equiprobable policy
+#simulate for 50 episodes
