60099face3e042b131eb069befb5e20979b06554
mapadilla1031 <55124740+mapadilla1031@users.noreply.github.com>
2023-11-27 23:20:37 -0700

    updates to initial Montecarlo setup
    update 1.1

--- PA3.py
+++ PA3.py
@@ -1,8 +1,13 @@
+import random
 #States
 states = ['R','T','D','U','8p']
 #Actions 
 actions = ['P', 'R', 'S','any']
+rewards = {'P': 2,'R': 0,'S': -1,'any': 1}
 
-#need method to get possible conbinations of rested-party ,rest,study,any
-#agent follows a random equiprobable policy
-#simulate for 50 episodes
+def monteCarlo():
+    for i in states:# possible actions and rewards for each state
+        action = random.choice(actions)        # use random to get an action 
+        reward = rewards[action]        # Get the reward based on the  random selected action
+        print(f"State: {i}, Action: {action}, Reward: {reward}")
+monteCarlo()
