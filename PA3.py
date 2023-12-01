bcad95e109e5d31382247560205f0a3458397eba
qhi6067 <jaime123perez43@gmail.com>
2023-12-01 10:39:23 -0700

    Update PA3.py

--- PA3.py
+++ PA3.py
@@ -1,38 +1,97 @@
+#update 1.5
 import random
 #States
-states = ['R','T','D','U','8p']
+states = ['RU 8p', 'TU 10p', 'RU 10p', 'RD 10p', 'RU 8a', 'RD 8a', 'TU 10a', 'RU 10a', 'RD 10a', 'TD 10a', '11am class']
 #Actions 
 actions = ['P', 'R', 'S','any'] #all actions are possible equally
 rewards = {'P': 2,'R': 0,'S': -1,'any': 1} #States to reward
+#Transition Probability
+MDP = {
+    'RU 8p': {
+        'P': {'TU 10p': {'prob': 1.0, 'reward': 2.0}},
+        'R': {'RU 10p': {'prob': 1.0, 'reward': 0.0}},
+        'S': {'RD 10p': {'prob': 1.0, 'reward': -1.0}}
+    },
+    'TU 10p': {
+        'P': {'RU 10a': {'prob': 1.0, 'reward': 2.0}},
+        'R': {'RU 8a': {'prob': 1.0, 'reward': 0.0}}
+    },
+    'RU 10p': {
+        'R': {'RU 8a': {'prob': 1.0, 'reward': 0.0}},
+        'P': {'RU 8a': {'prob': 0.5, 'reward': 2.0}, 'RU 10a': {'prob': 0.5, 'reward': 2.0}},
+        'S': {'RD 8a': {'prob': 1.0, 'reward': 0.0}}
+    },
+    'RD 10p': {
+        'R': {'RU 8a': {'prob': 1.0, 'reward': 0.0}},
+        'P': {'RD 8a': {'prob': 0.5, 'reward': 2.0}, 'RD 10a': {'prob': 0.5, 'reward': 2.0}}
+    },
+    'RU 8a': {
+        'P': {'TU 10a': {'prob': 1.0, 'reward': 2.0}},
+        'R': {'RU 10a': {'prob': 1.0, 'reward': 0.0}},
+        'S': {'RD 10a': {'prob': 1.0, 'reward': -1.0}}   
+    },
+    'RD 8a': {
+        'R': {'RD 10a': {'prob': 1.0, 'reward': 0.0}},
+        'P': {'TD 10a': {'prob': 1.0, 'reward': 2.0}}
+    },
+    'TU 10a': {
+        'any': {'11am class': {'prob': 1.0, 'reward': -1.0}}
+    },
+    'RU 10a': {
+        'any': {'11am class': {'prob': 1.0, 'reward': 0.0}}
+    },
+    'RD 10a': {
+        'any': {'11am class': {'prob': 1.0, 'reward': 4.0}}
+    },
+    'TD 10a': {
+        'any': {'11am class': {'prob': 1.0, 'reward': 3.0}}
+    }
+}
+
+
 stateVal = {state: 0 for state in states} # states initialized to 0
 
 #learning rate (alpha)0.1 per part 1; 0.2 per part 3 (Q learning)
 alpha = 0.1
 
+#modification to choose next state based on transition probabilities
 def monteCarlo(episodes):
-    totalReward = 0
     for episode in range(episodes):
         currentState = random.choice(states)
-        episodeCurrentReward = 0
+        episodeRewards = 0
         visited = set()
+        stateActionPairs = []
 
-        while currentState != '8p':
-            action = random.choice(actions) #performs random actino
-            reward = rewards[action] #get dictionary value of rewards based on action
-            episodeCurrentReward += reward #adding reward
-            visited.add(currentState) #adding to visited set 
-            currentState = random.choice(states) # chooxinga random state
-            
-            #Performing first visit Monte Carlo  with alpha learning rate of 0.1
-            for state in visited:
-                stateVal[state] = stateVal[state] + alpha * (episodeCurrentReward - stateVal[state])
+        while currentState != '11am class':
+            # Debugging print 
+            #print(f"Current State: {currentState}")
 
-            totalReward += episodeCurrentReward
-        averageReward = totalReward/episodes #dividie total rewards by num of iterations. Denoted as episodes, for part 1 (50 episodes)
-        print(f"Final State value: {stateVal} \n Average Reward: {averageReward}")
+            available_actions = [action for action in actions if action in MDP[currentState]]
+            if not available_actions:
+                #Debugging print
+                #print(f"No available actions for state: {currentState}")
+                break  # Exit loop if no actions available
 
-    #for i in states:# possible actions and rewards for each state
-        #action = random.choice(actions)        # use random to get an action 
-        #reward = rewards[action]        # Get the reward based on the  random selected action
-        #print(f"State: {i}, Action: {action}, Reward: {reward}")
-monteCarlo(50) #calling 50 episodes per part one
+            action = random.choice(available_actions)
+            nextState, transition = list(MDP[currentState][action].items())[0]
+            reward = transition['reward']
+            episodeRewards += reward
+            stateActionPairs.append((currentState, action, reward))
+
+            currentState = nextState
+
+        # First-visit Monte Carlo updates
+        for state, action, reward in stateActionPairs:
+            if state not in visited:
+                stateVal[state] += alpha * (episodeRewards - stateVal[state])
+                visited.add(state)
+
+        # Print the episode's sequence of experience and rewards
+        print(f"Episode {episode}: {stateActionPairs}, Total Reward: {episodeRewards}")
+
+    # Calculate and print average reward
+    averageReward = sum(episodeRewards for _ in range(episodes)) / episodes
+    print(f"Final State Values: {stateVal}, Average Reward: {averageReward}")
+
+monteCarlo(50)
+#need to print to Print out the values of all of the states at the end of your exp
